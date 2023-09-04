02ede9399382fffd7abc9aaa87bd84f4c129d38b
gayashan4lk <gayashan4lk@gmail.com>
2023-09-05 04:28:43 +0530

    specify data provider

--- ch02-basics/PublisherData/Class1.cs
+++ ch02-basics/PublisherData/Class1.cs
@@ -6,7 +6,11 @@ namespace PublisherData
 	public class PubContext:DbContext
 	{
 		public DbSet<Author> Authors { get; set; }
-
 		public DbSet<Book> Books { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase");
+		}
 	}
 }
